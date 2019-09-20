73ee16ec53fe7f3f7696ec5dea658e03c6fe3178
timeoutme <383480701@qq.com>
2019-09-20 10:59:00 +0800

    添加了源文件

---
+++ mysite/views.py
@@ -0,0 +1,13 @@
+# from django.http import HttpResponse
+from django.shortcuts import render 
+from collections import Counter
+
+def index(request):
+    return render(request,'home.html')
+
+def count(request):
+    counts = (len(request.GET['text'])) #统计提交文本字数
+    content = request.GET['text']   #获取提交文本
+    frequency = dict(Counter(content))
+    frequency = sorted(frequency.items(),key=lambda x:x[1],reverse=True)
+    return render(request,'count.html',{'counts':counts,'content':content,'frequecy':frequency})    
